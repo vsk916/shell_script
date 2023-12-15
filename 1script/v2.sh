7090a8f4daee3d28e1c9594d798118b2ee81e8d6
vsk916 <pentelavamsisaikrishna@gmail.com>
2023-12-15 13:29:10 +0530

    save changes

---
+++ 1script/v2.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo "Ramesh: : Hello suresh, Good morning"
+echo "suresh: : Hi Ramesh, very Good morning"
+echo "Ramesh: : How are you Suresh?"
+echo "Suresh: : I am good Ramesh, How are you"
+
+
+# This is for testing
